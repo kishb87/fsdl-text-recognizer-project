355f4761c45ac11001e0ed95c8636364fb22561b
kishb87 <kishb87@local>
2018-08-03 23:09:46 +0000

    lab1 submission 2

--- lab1/text_recognizer/networks/mlp.py
+++ lab1/text_recognizer/networks/mlp.py
@@ -1,8 +1,10 @@
 from typing import Tuple
 
+import tensorflow as tf
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
-
+from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, Input, Lambda, MaxPooling2D
+from tensorflow.keras.models import Sequential, Model
 
 def mlp(input_shape: Tuple[int, ...],
         output_shape: Tuple[int, ...],
@@ -18,10 +20,22 @@ def mlp(input_shape: Tuple[int, ...],
     model = Sequential()
     # Don't forget to pass input_shape to the first layer of the model
     ##### Your code below (Lab 1)
-    model.add(Flatten(input_shape=input_shape))
-    for _ in range(num_layers):
-        model.add(Dense(layer_size, activation='relu'))
-        model.add(Dropout(dropout_amount))
+    # model.add(Flatten(input_shape=input_shape))
+    # for _ in range(num_layers):
+        # model.add(Dense(layer_size, activation='relu'))
+        # model.add(Dropout(dropout_amount))
+    # model.add(Dense(num_classes, activation='softmax'))
+    
+    if len(input_shape) < 3:
+        model.add(Lambda(lambda x: tf.expand_dims(x, -1), input_shape=input_shape))
+        input_shape = (input_shape[0], input_shape[1], 1)
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.2))
     model.add(Dense(num_classes, activation='softmax'))
     ##### Your code above (Lab 1)
 
